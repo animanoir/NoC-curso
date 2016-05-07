a3fc1ad1a45d534f30d2861774fe35cf085181e9
Oscar <etherealflux94@gmail.com>
2016-05-06 22:31:50 -0500

    Añadi la carpeta de mi asignatura

--- randomWalker/sketch.js
+++ randomWalker/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(640, 360); //Crea el canvas en HTML para que se vea todo.
+  createCanvas(800, 600); //Crea el canvas en HTML para que se vea todo.
   w = new Walker();
 
 }
@@ -14,12 +14,17 @@ function draw() {
 function Walker() {
   this.pos = createVector(width / 2, height / 2); //Función que sirve para crear un vector con dos variables, x y y.
   this.vel = createVector(0, 0); //Velocidad
+  this.acc = p5.Vector.fromAngle(random(TWO_PI));
+  this.acc.setMag(0.2);
 
 //Esto es una función para que camine.
 this.walk = function() {
-  var mouse = createVector(mouseX, mouseY);
-  this.acc = p5.Vector.sub(mouse, this.pos); //La diferencia entre la posisión del objeto w (vector) y la posición del mouse (otro vector).
-  this.acc.mult(0.1); //Se múltiplca la aceleración por un escalar.
+  //var mouse = createVector(mouseX, mouseY);
+  //this.acc = p5.Vector.sub(mouse, this.pos); //La diferencia entre la posisión del objeto w (vector) y la posición del mouse (otro vector).
+  //this.acc.mult(0.01); //Se múltiplca la aceleración por un escalar. (ya sea para aumentar o disminuir su potencia).
+  //this.acc.normalize(1); //'Normaliza' a 1 (fuerza a que la magnitud sea siempre de 1, independientemente de donde se localice el vector. )
+  //this.acc.setMag(0.5);
+  
   this.vel.add(this.acc);
   this.pos.add(this.vel);
 
---
+++ session1_asignatura/Walker.js
@@ -0,0 +1,8 @@
+//Clase Walker.
+
+function Walker(){
+  this.pos = createVector(width/2, height/2); //Posición (inicial).
+  this.vel = createVector(0, 0); //Velociddad.
+  this.acc = createVector(); //Aceleración.
+  
+}
