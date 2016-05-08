774d71eba9608f4e0b64065975296a1f4867f1e0
Oscar <etherealflux94@gmail.com>
2016-05-07 22:11:35 -0500

    Hice muchas cosas

--- asignatura1/sketch.js
+++ asignatura1/sketch.js
@@ -4,6 +4,6 @@ function setup() {
 }
 
 function draw() {
-  fill(255);
-  ellipse(mouseX, mouseY, 20, 20);
+
 }
+
--- session1_asignatura/Walker.js
+++
@@ -1,8 +0,0 @@
-//Clase Walker.
-
-function Walker(){
-  this.pos = createVector(width/2, height/2); //Posición (inicial).
-  this.vel = createVector(0, 0); //Velociddad.
-  this.acc = createVector(); //Aceleración.
-  
-}
