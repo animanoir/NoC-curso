7fb16ac7d0cfcd633cae9745218a47d8d94a1d55
Oscar <etherealflux94@gmail.com>
2016-05-08 11:59:10 -0500

    Quité algunos espacios

--- asignatura1/sketch.js
+++ asignatura1/sketch.js
@@ -1,10 +1,33 @@
+//Óscar A. Montiel
+//The Nature of Code en Kadenze
+//twitter: @_geosmina
+
+var w;
+
 function setup() {
+  //Escenario
   createCanvas(800, 600);
   background(0);
+  
+  //Objetos
+  w = new Walker();
+  
 }
 
 function draw() {
-  fill(255);
-  ellipse(mouseX, mouseY, 10, 10);
+  w.display();
+}
+
+//Walker class
+function Walker(){
+  //Crea un Walker en una parte al azar del canvas.
+  this.pos = createVector(random(width), random(height));
+  
+  //Funciones
+  this.display = function(){
+    //Dibuja un círculo como un Walker
+    fill(random(255), random(255), random(255));
+    ellipse(this.pos.x, this.pos.y); //this.pos.x y .y se refiere a los atributos x y y de un createVector.
+  };
 }
 
