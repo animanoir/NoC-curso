6bc1b11e8d927461863225d5441412e61997b8ac
Oscar <etherealflux94@gmail.com>
2016-05-08 21:32:26 -0500

    Versión 1

--- asignatura1/sketch.js
+++ asignatura1/sketch.js
@@ -1,33 +1,55 @@
 //Óscar A. Montiel
 //The Nature of Code en Kadenze
 //twitter: @_geosmina
+//github: https://animanoir.github.io/
 
-var w;
+//Crea un arreglo.
+var walkers = new Array(250);
 
 function setup() {
   //Escenario
   createCanvas(800, 600);
-  background(0);
-  
-  //Objetos
-  w = new Walker();
-  
+  background(random(255), random(255), random(255));
+  smooth(); //Le pone antialias, creo.
+
+  //Inicializa objetos en el arreglo.
+  for (var i = 0; i < walkers.length; i++) {
+    walkers[i] = new Walker();
+  }
+
 }
 
 function draw() {
-  w.display();
+  fill(0, 10);
+  rect(0, 0, width, height);
+  for (var i = 0; i < walkers.length; i++) {
+    walkers[i].display();
+    walkers[i].update();
+  }
 }
 
 //Walker class
-function Walker(){
+function Walker() {
   //Crea un Walker en una parte al azar del canvas.
   this.pos = createVector(random(width), random(height));
+  var c = color(random(255), random(255), random(255));
   
+
   //Funciones
-  this.display = function(){
+  this.update = function () {
+    //Mueve el objeto al azaar.
+    var vel = createVector(random(-1, 1), random(-1, 1));
+    this.pos.add(vel);
+  };
+
+  this.display = function () {
+    var escalar = random(20);
+    var n = noise(millis()) * escalar;
+    print(n);
     //Dibuja un círculo como un Walker
-    fill(random(255), random(255), random(255));
-    ellipse(this.pos.x, this.pos.y); //this.pos.x y .y se refiere a los atributos x y y de un createVector.
+    noStroke();
+    fill(c);
+    ellipse(this.pos.x, this.pos.y, n, n); //this.pos.x y .y se refiere a los atributos x y y de un createVector.
   };
 }
 
