436d794ad034f0729eb4c20cd70be32957575036
Oscar <etherealflux94@gmail.com>
2016-05-20 17:42:09 -0500

    Nueva carpeta

---
+++ asignatura2/sketch.js
@@ -0,0 +1,13 @@
+//Óscar A. Montiel
+//The Nature of Code en Kadenze (Course, homework 2).
+//twitter: @_geosmina
+//github: https://animanoir.github.io/
+
+function setup() {
+
+}
+
+function draw() {
+ 
+}
+
