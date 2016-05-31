40d7a5cfe0ec937df945a01a5130c80933987011
Oscar <etherealflux94@gmail.com>
2016-05-31 00:16:47 -0500

    Algo mega rapido que ya se me hacía tarde

--- asignatura2/particula.js
+++ asignatura2/particula.js
@@ -1,30 +1,43 @@
-function Particle () {
-	this.pos = createVector(random(800), random(600));
+function Particula (x, y, m) {
+	this.pos = createVector(x, y);
 	this.vel = createVector(0, 0);
 	this.acc = createVector(0, 0);
+	this.mass = 1; //Escalar
 
-	
+	//Functions
 
-	
-	//Funtions
+	//Apply a force, for the object to move in an x or y axis (or both).
 	this.applyForce = function(force){
-		this.acc = force;
+		var f = force.copy(); //Crea una copia de force para que no afecte la variable original
+		//a=(∑F)/m).
+		force.div(this.mass);
+		this.acc.add(force);
 	};
 
 	this.update = function(){
 		this.vel.add(this.acc);
 		this.pos.add(this.vel);
+
+		this.acc.set(0, 0);
 	};
 
-	this.display = function(){
-		fill(random(255), random(255), random(255));
-		ellipse(this.pos.x, this.pos.y, 50, 50);
+	this.display = function(r, g, b){
+		noStroke();
+		fill(r, g, b, 182);
+		ellipse(this.pos.x, this.pos.y, m*this.mass, m*this.mass);
 	};
 
-	this.checkIfOutCanvasY = function(){
+	this.checkIfOutCanvasXY = function(){
 		if(this.pos.y > height){
-			this.pos.y = 0;
-			this.pos.x = random(width);
+			this.vel.y *= -1;
+			this.pos.y = height;
+		}
+		if(this.pos.x > width){
+			this.vel.x *= -1;
+			this.pos.x = width;
+		}else if(this.pos.x < 0){
+			this.vel.x *= -1;
+			this.pos.x = 0;
 		}
 	};
 }
--- asignatura2/sketch.js
+++ asignatura2/sketch.js
@@ -3,20 +3,52 @@
 //twitter: @_geosmina
 //github: https://animanoir.github.io/
 
-var particula;
+
+var particulasRojas = [];
+var particulasAzules = [];
+var cantidad = 200;
 
 function setup() {
-	particula = new Particle();
+	for (var i = 0; i < cantidad; i++) {
+		particulasRojas[i] = new Particula(random(800), random(600), random(10, 50));
+	}
+	for (var i = 0; i < cantidad; i++) {
+		particulasAzules[i] = new Particula(random(800), random(600), random(10, 50));
+	}
 
 	createCanvas(800, 600);
-	background(0);
+
 }
 
 function draw() {
-	var gravity = createVector(0, 0.1);
-	particula.applyForce(gravity);
-	particula.display();
-	particula.checkIfOutCanvasY();
-	particula.update();
+	background(0);
+	//Gravity variable
+	var gravity = createVector(0, 1);
+	var windRed = createVector(0.2, 1);
+	var windBlue = createVector(-0.2, 1);
+
+	for (var i = 0; i < particulasRojas.length; i++) {
+		particulasRojas[i].applyForce(gravity);
+		particulasRojas[i].display(255, 0, 0);
+		particulasRojas[i].checkIfOutCanvasXY();
+		particulasRojas[i].update();
+	}
+	for (var i = 0; i < particulasAzules.length; i++) {
+		particulasAzules[i].applyForce(gravity);
+		particulasAzules[i].display(0, 0, 255);
+		particulasAzules[i].checkIfOutCanvasXY();
+		particulasAzules[i].update();
+	}
+
+	if (mouseIsPressed) {
+		for (var i = 0; i < particulasRojas.length; i++) {
+			particulasRojas[i].applyForce(windRed);
+			particulasAzules[i].applyForce(windBlue);
+		}
+	}
+}
+
+function keyPressed(){
+	setup();
 }
 
