889c51eaa28764a4d72695bbcb7d3495b9be7f16
Oscar <etherealflux94@gmail.com>
2016-06-12 23:01:55 -0500

    Tarea 3 terminada

--- asignatura3/sketch.js
+++ asignatura3/sketch.js
@@ -1,75 +1,55 @@
-//Óscar A. Montiel
-//The Nature of Code, (Course, homework 3).
-//twitter: @_geosmina
+//Code by Dan Shiffman, adapted by Óscar A. Montiel
 
-//Used code for 2D water simulation by http://www.openprocessing.org/user/25336
+//Array to store all the new foods
+var foods = [];
+var fish = [];
 
-var numCols = 160;
-var water = [];
-var ten = 0.025;
-var damp = 0.065;
-var spread = 0.001;
-var stone;
-
+// Liquid
+var liquid;
 
 function setup() {
-  createCanvas(800, 600); //Creates the canvas where everything will happen.
 
-  for(var i=0; i<numCols; i++){
-    water[i] = new Water(i*5, 500, 240, 240, 0);
-  }
+  createCanvas(800, 600);
+  // Create liquid object
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
 
-  stone = new Stone(createVector(random(width), -30));
+  
 }
 
 function draw() {
-  background(0);
-  stone.run();
+  fill(0, 10);
+  rect(0, 0, width, height);
 
-  if (stone.pos.y>240) {
-    stone.pos.y=200-random(1000);
-    stone.pos.x=random(width);
-    stone.vel.y=0;
-    stone.alpha=255;
-  }
+  // Draw water
+  liquid.display();
+
+  for(var i = 0; i<foods.length; i++){
+
+    /*if(liquid.contains(foods[i])){
+      var dragForce = liquid.calculateDrag(foods[i]);
+      foods[i].applyForce(dragForce);
+    }
+
+    */
+
+    var gravity = createVector(0, 0.01 * foods[i].mass);
+
+    foods[i].applyForce(gravity);
   
-  for(var i=0; i<numCols; i++){
+    foods[i].update();
+    foods[i].display();
+    //foods[i].checkEdges();
 
-    water[i].display();
-    water[i].update(damp, ten);
+    var target = createVector(foods[i].position.x, foods[i].position.y);
+    fish[i].arrive(target);
 
-    if (stone.pos.y < 240 && stone.pos.y + stone.vel.y >= 240) {
-
-      water[round(stone.pos.x/5)].h = stone.vel.y * stone.vel.y*5;
-      console.log("Works.");
-    }
-
-
-    var lDeltas = new Array(numCols);
-    var rDeltas = new Array(numCols);
-
-    for(var j=0; j<8; j++){
-
-      for(var l=0;l<numCols; l++){
-
-        if(l > 0){
-          lDeltas[l] = spread * (water[l].h - water[l-1].h);
-          water[l-1].spd += lDeltas[l];
-        }
-        if(l < numCols-1){
-          rDeltas[l] = spread * (water[l].h - water[l+1].h);
-          water[l+1].spd += rDeltas[l];
-        }
-      }
-
-      for(var r=0; r<numCols; r++){
-        if(r > 0){
-          water[r-1].h += lDeltas[r];
-        }
-        if(r < numCols - 1){
-          water[r+1].h += rDeltas[r];
-        }
-      }
-    }
+    fish[i].update();
+    fish[i].display();
   }
+}
+
+function mouseDragged(){
+  //Create new Food object and add it to the array foods
+  foods.push(new Food(random(0.5, 2), mouseX, mouseY)); //mass, x, y
+  fish.push(new Fish(random(width), random(height), 1, random(255), random(255), random(255)));
 }
--- asignatura3/vehicle.js
+++ asignatura3/vehicle.js
@@ -1,38 +1,70 @@
-//Vehicle Class
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 3: Arrive Steering Behavior
 
-function Vehicle(x, y, m){
-	this.pos = createVector(x, y);
-	this.vel = createVector(0, 0);
-	this.acc = createVector(0, 0);
-	this.maxSpeed = 5;
-	this.maxForce = 0.04;
+function Fish(x, y, m, r, g, b) {
+  this.pos = createVector(x, y);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 5;
+  this.maxforce = 0.2;
+  this.r = 0;
+  this.g = 0;
+  this.b = 0;
 
 
-	//Functions
+  this.r = 6;
 
-	this.applyForce = function(force){
-		this.acc.add(force);
-	};
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
 
-	//Move around.
-	this.update = function(){
-		this.vel.add(this.acc);
-		this.vel.limit(this.maxSpeed);
-		this.pos.add(this.vel);
-		this.acc.set(0, 0);
-	};
+  this.arrive = function(target) {
 
-	this.display = function(){
-		fill(255, 150);
-		stroke(255);
-		ellipse(this.pos.x, this.pos.y, 48, 48);
-	};
+    var desired = p5.Vector.sub(target, this.pos);
 
-	this.seek = function(target){
-		var desired = p5.Vector.sub(target, this.pos);
-		desired.setMag(this.maxSpeed); //Scalar that sets the magnitude of the vector.
-		var steering = p5.Vector.sub(desired, this.vel);
-		steering.limit(this.maxForce);
-		this.applyForce(steering);
-	};
+    // desired.setMag(this.maxspeed);
+
+    // The arrive behavior!
+    var d = desired.mag();
+
+    if (d < 100) {
+      // Map the desired magnitude according to distance
+      var m = map(d, 0, 100, 0, this.maxspeed);
+      desired.setMag(m);
+    } else {
+      desired.setMag(this.maxspeed);
+    }
+
+
+    var steering = p5.Vector.sub(desired, this.vel);
+    steering.limit(this.maxforce);
+
+    this.applyForce(steering);
+
+  }
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  this.display = function() {
+    // Draw a triangle rotated in the direction of velocity
+    var theta = this.vel.heading() + PI / 2;
+    fill(r, g, b);
+    noStroke();
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(theta);
+    beginShape();
+    vertex(0, -this.r * 2);
+    vertex(-this.r, this.r * 2);
+    vertex(this.r, this.r * 2);
+    endShape(CLOSE);
+    pop();
+  }
 }
