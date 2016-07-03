43ba51eddb2ad20a255f1404231f517634fe9c06
Oscar <etherealflux94@gmail.com>
2016-07-03 15:01:56 -0500

    Nueva asignatura

---
+++ asignatura4/sketch.js
@@ -0,0 +1,15 @@
+//Óscar A. Montiel
+//The Nature of Code en Kadenze (Course, homework 4).
+//twitter: @_geosmina
+//github: https://animanoir.github.io/
+
+function setup() {
+  //Escenario
+  createCanvas(800, 600);
+  background(0);
+
+}
+
+function draw() {
+ 
+}
