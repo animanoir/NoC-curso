73fb73ee890a95bc82e6d74d0d6622f244d70e04
Oscar <etherealflux94@gmail.com>
2016-07-27 23:20:56 -0500

    Nuevo folder para lo de los algoritmos genéticos

---
+++ asignatura5/sketch.js
@@ -0,0 +1,13 @@
+//Óscar A. Montiel
+//The Nature of Code en Kadenze (Course, homework 5).
+//twitter: @_geosmina
+//github: https://animanoir.github.io/
+
+function setup() {
+
+}
+
+function draw() {
+
+
+}
